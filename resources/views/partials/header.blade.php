564d05e9703f7bc04bb0ef3cccbd97444496cb65
xTavi <41364089+xTavi@users.noreply.github.com>
2018-07-18 15:20:19 +0300

    Centralizare logo in header + padding

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,33 +1,41 @@
-@if(Auth::check())
-    <ul class="nav justify-content-end">
-    @if(Auth::user()->hasRole('Admin'))
-        <li class="nav-item">
-            <a class="nav-link active" href="/admin">Admin</a>
-        </li>
-        @elseif(Auth::user()->hasRole('Manager'))
-        <li class="nav-item">
-            <a class="nav-link active" href="/manager">Manager</a>
-        </li>
-    @elseif(Auth::user()->hasRole('Developer'))
-        <li class="nav-item">
-            <a class="nav-link active" href="/developer">Developer</a>
-        </li>
-    @elseif(Auth::user()->hasRole('Client'))
-        <li class="nav-item">
-            <a class="nav-link active" href="/client">Client</a>
-        </li>
-    @endif
-    <li class="nav-item">
-        <a class="nav-link active" href="/logout">LogOut</a>
-    </li>
-    </ul>
-@else
-    <ul class="nav justify-content-end">
-        <li class="nav-item">
-            <a class="nav-link active" href="/signup">Register</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/signin">Login</a>
-        </li>
-    </ul>
-@endif
+<ul class="nav justify-content-end">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-9 col-sm-9 col-xs-9">
+                <div class="Logo">
+                    <img src="https://brilliantdevs.com/wp-content/uploads/2017/06/logo-icon-big-black-150x150.png" id="logo">
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-3" id="positionnav">
+                @if(Auth::check()) @if(Auth::user()->hasRole('Admin'))
+                <li class="nav-item">
+                    <a class="nav-link active" href="/admin">Admin</a>
+                </li>
+                @elseif(Auth::user()->hasRole('Manager'))
+                <li class="nav-item">
+                    <a class="nav-link active" href="/manager">Manager</a>
+                </li>
+                @elseif(Auth::user()->hasRole('Developer'))
+                <li class="nav-item">
+                    <a class="nav-link active" href="/developer">Developer</a>
+                </li>
+                @elseif(Auth::user()->hasRole('Client'))
+                <li class="nav-item">
+                    <a class="nav-link active" href="/client">Client</a>
+                </li>
+                @endif
+                <li class="nav-item">
+                    <a class="nav-link active" href="/logout">LogOut</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link active" href="/signup">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/signin">Login</a>
+                </li>
+                @endif
+            </div>
+        </div>
+    </div>
+</ul>
