4f18cf584a7db9122ee30f4a2d95f29e5ab60d4b
xTavi <41364089+xTavi@users.noreply.github.com>
2018-07-19 14:17:59 +0300

    Modificare header in functie de toti oamenii

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,23 +2,41 @@
 <ul class="nav justify-content-end">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-9 col-sm-9 col-xs-9">
+            <div class="col-md-8 col-sm-8 col-xs-8">
                 <div class="Logo">
                     <a href="/"><img src="https://brilliantdevs.com/wp-content/uploads/2017/06/logo-icon-big-black-150x150.png" id="logo"></a>
                 </div>
             </div>
-            <div class="col-md-3 col-sm-3 col-xs-3" id="positionnav">
+            <div class="col-md-4 col-sm-4 col-xs-4" id="positionnav">
                 @if(Auth::check()) @if(Auth::user()->hasRole('Admin'))
                 <li class="nav-item">
                     <a class="nav-link active" href="/admin">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/manager">Manager</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/developer">Developer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/client">Client</a>
                 </li>
                 @elseif(Auth::user()->hasRole('Manager'))
                 <li class="nav-item">
                     <a class="nav-link active" href="/manager">Manager</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/developer">Developer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/client">Client</a>
+                </li>
                 @elseif(Auth::user()->hasRole('Developer'))
                 <li class="nav-item">
                     <a class="nav-link active" href="/developer">Developer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/client">Client</a>
                 </li>
                 @elseif(Auth::user()->hasRole('Client'))
                 <li class="nav-item">
