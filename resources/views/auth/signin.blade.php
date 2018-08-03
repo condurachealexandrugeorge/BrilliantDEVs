6511d8cfdf051fa33bd13974ac1433a006a434ae
ciprian <condurachealexandrugeorge@gmail.com>
2018-08-03 17:11:13 +0300

    Edit login style

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -5,14 +5,14 @@
         <!-- Default form login -->
         <form class="text-center border border-light p-5" action="{{ route('signin') }}" method="post" style="border: none!important;width: 100%; max-width: 500px; padding: 15px; margin: auto;">
             <img src="{{ asset('img/logo.png') }}" class="img-fluid" alt="" style="max-height: 70px;">
-            <p class="h4 mb-4">Sign in</p>
+            <p class="h4 mb-4" style="margin-top: 30px ">Please sign in</p>
             <!-- Email -->
             <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail" name="email">
             <!-- Password -->
             <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password" name="password">
         {{ csrf_field() }}
         <!-- Sign in button -->
-            <button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+            <button class="btn btn-info btn-block my-4" type="submit" style="background-color: #fc685f!important;">Sign in</button>
         </form>
     </div>
     <!-- Default form login -->
